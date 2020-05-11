8dc6ed025219ca22d8016806b50cfb1c37fcdbcc
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-11 01:42:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(attendees)
   array = [ ] #Array created to put names inside
   attendees.each {|name|array << badge_maker(name)} #loop through each name provided and push the message to new array with the name
-  return
+  array
 end
 
 def assign_rooms(attendees)
