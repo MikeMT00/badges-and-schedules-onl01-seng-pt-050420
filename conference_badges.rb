e0c3566b1c766804eaaed950157aeb6144a2a027
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-11 01:40:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,12 @@ def batch_badge_creator(attendees)
   return
   end
 end
+
+def assign_rooms(attendees)
+   array = []
+   #each_with_index: keep track of the index number of the current iteration
+   attendees.each_with_index do |name, index|
+      array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+   end
+   array
+end
